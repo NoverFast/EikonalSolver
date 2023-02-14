018a33111c5823665d153fb2802793068b30580b
Nover <onverx@gmail.com>
2023-02-14 12:24:28 +0500

    Добавьте файлы проекта.

---
+++ EikonalSolver/Forms/MainForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace EikonalSolver
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.BackColor = System.Drawing.SystemColors.ControlDark;
+      this.ClientSize = new System.Drawing.Size(1006, 465);
+      this.Name = "MainForm";
+      this.Text = "Уравнение эйконала";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
